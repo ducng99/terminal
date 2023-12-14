89234de0cc820e9200f31212ceae85bd30ef38af
ducng99 <tom@ducng.dev>
2023-12-15 10:50:42 +1300

    feat: Better user input command parsing
    Plus changes to boot sequence

--- src/shell.js
+++ src/shell.js
@@ -1,10 +1,16 @@
 import { loadScript } from "./utils.js";
 
+/**
+ * Command to exit the shell
+ */
+const EXIT_COMMAND = "exit";
+
 // List of commands and their functions
 /** @type {{ [key: string]: (args: string[]) => Promise<any> | any }} */
 let commands = {};
 
 export async function loadShell() {
+    // Expose functions to the window
     window.s_registerCommand = registerCommand;
     window.s_getCommands = getCommands;
 
@@ -13,64 +19,73 @@ export async function loadShell() {
 }
 
 async function bootSequence() {
-    await s_print("Welcome to OldNet terminal.", { preDelay: 2000, postDelay: 2000 });
+    await s_print("BOOTING...", { preDelay: 2000, postDelay: 2000 });
     await s_print("\n\n", { printDelay: 0 });
-    await s_print("Begin boot sequence", { postDelay: 1000 });
-
-    await s_print("\n", { printDelay: 0 });
 
     // Load default commands
     {
         let defaultCommandsLoaded = false;
-        let defaultCommandsError = false;
+        let defaultCommandsLoadError = false;
 
         loadDefaultCommands()
-            .then(() => { defaultCommandsLoaded = true })
             .catch(() => {
+                defaultCommandsLoadError = true;
+            })
+            .finally(() => {
                 defaultCommandsLoaded = true;
-                defaultCommandsError = true
             });
 
-        await s_print("Loading default apps...");
+        await s_print("LOADING DEFAULT PROGRAMS...");
 
         while (!defaultCommandsLoaded) {
             await s_print(".", { postDelay: 500 });
         }
 
-        if (defaultCommandsError) {
+        if (defaultCommandsLoadError) {
             await s_print(" ERROR");
         } else {
             await s_print(" OK");
         }
     }
 
-    await s_print("\n", { printDelay: 0 });
-    await s_print("Boot sequence completed", { postDelay: 1000 });
+    await s_print("\n\n", { printDelay: 0 });
+    await s_print("BOOT COMPLETED!", { postDelay: 2000 });
+    await s_clear();
+
+    await s_print("Welcome to Old NET.", { preDelay: 1000, postDelay: 500 });
+    await s_print("\nType 'help' to get started.", { postDelay: 1000 });
+    await s_print("\n\n", { printDelay: 0 });
 }
 
 async function beginShell() {
     let user_input = "";
 
-    while (user_input !== "exit") {
+    while (user_input !== EXIT_COMMAND) {
         try {
             user_input = await s_prompt("> ");
-            if (user_input === "") continue;
-            const [command, ...args] = user_input.split(" ");
+            if (user_input === "" || user_input === EXIT_COMMAND) continue;
 
+            const { command, args } = parseCommand(user_input);
             await runCommand(command, args);
         } catch (ex) {
             // Do nothing when user cancels
         }
     }
+
+    await s_print("Goodbye!", { postDelay: 2000 });
 }
 
 /**
- * Add a command to the shell
+ * Add a command to the shell. If a command with the same name already exists, it will NOT be overwritten.
  * @param {string} name Name of the command
  * @param {(args: string[]) => Promise<void>} func Function to run when the command is called
  */
 function registerCommand(name, func) {
-    commands[name] = func;
+    if (name in commands) {
+        console.warn(`Command already exists: ${name}`);
+    } else {
+        commands[name] = func;
+    }
 }
 
 /**
@@ -92,6 +107,28 @@ async function runCommand(name, args) {
     } else {
         await s_print(`Command not found: ${name}\n`);
     }
+}
+
+/**
+ * Parses a command string into a command and arguments
+ * @param {string} commandString Command string to parse
+ * @returns {{ command: string, args: string[] }} Command and arguments
+ */
+function parseCommand(commandString) {
+    // Regular expression to match command and arguments
+    // This will match either a sequence of non-space, non-quote characters,
+    // or a sequence of characters inside double quotes (supporting escaped quotes)
+    var regex = /[^\s"']+|"([^"\\]*(\\.[^"\\]*)*)"|'([^'\\]*(\\.[^'\\]*)*)'/g;
+    var parts = commandString.match(regex);
+
+    // The first part is the command
+    var command = parts[0];
+
+    // The rest are the arguments
+    // Remove quotes from arguments
+    var args = parts.slice(1).map(arg => arg.startsWith('"') ? arg.slice(1, -1).replace(/\\"/g, '"') : arg);
+
+    return { command, args };
 }
 
 /**
