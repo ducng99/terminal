7d0c64af913a41af845a2b3587dca872e8e30a3d
ducng99 <tom@ducng.dev>
2023-12-16 02:39:14 +1300

    Flashing caret in title

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ import { setupScreen } from './src/screen';
 import { loadShell } from './src/shell';
 import './style.css'
 
+let titleShowingCursor = false;
+setInterval(function() {
+    document.title = (titleShowingCursor = !titleShowingCursor) ? "Terminal" : "Terminal■";
+}, 500);
+
 document.querySelector('#app').innerHTML = `
   <div id="overlay"></div>
   <div id="screen"></div>
