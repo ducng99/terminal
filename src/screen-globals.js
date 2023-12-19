6acb33bcaa805e0c150f4fb988e4dfc572516fd1
ducng99 <tom@ducng.dev>
2023-12-19 16:16:56 +1300

    Remove old file

--- src/screen-globals.js
+++
@@ -1,9 +0,0 @@
-export class PromptCancelEvent extends CustomEvent {
-    constructor(options = {}) {
-        super('cancel', {
-            detail: {
-                remove: options.remove
-            }
-        });
-    }
-}
